cc39395eeca9dd23451fdfdecb2755d1ffa8b924
seif mohamed <50503743+seifmohamed0@users.noreply.github.com>
2022-10-09 02:14:55 +0200

    Time: 23 ms (5.07%), Space: 41.7 MB (22.93%) - LeetHub

---
+++ 202-happy-number/202-happy-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int rep = 1000;
+        long prv = n;
+        while(rep > 0){
+            --rep;
+            long curr = 0;
+            while(prv > 0){
+                int p = (int)(prv % 10);
+                prv /= 10;
+                curr += p * p;
+            }
+            if(curr == 1)
+                return true;
+            prv = curr;
+        }
+        return false;
+    }
+}
