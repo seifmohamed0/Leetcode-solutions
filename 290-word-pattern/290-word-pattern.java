ea84bbceef85ded57556ebbe6ba3be7ffd3f1791
seif mohamed <50503743+seifmohamed0@users.noreply.github.com>
2022-10-09 03:09:41 +0200

    Time: 1 ms (98.09%), Space: 42.5 MB (5.25%) - LeetHub

--- 290-word-pattern/290-word-pattern.java
+++ 290-word-pattern/290-word-pattern.java
@@ -1,37 +1,12 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        String[] map = new String[26];
-        int ptr1 = 0 , ptr2 = 0;
-        int sz1 = pattern.length();
-        int sz2 = s.length();
-        HashMap<String , Character> mp= new HashMap<String , Character>();
-        while(ptr1 < sz1 && ptr2 < sz2){
-            StringBuilder new_s = new StringBuilder();
-            while(ptr2 < sz2 && s.charAt(ptr2) != ' '){
-                new_s.append(s.charAt(ptr2));
-                ++ptr2;
-            }
-            ++ptr2;
-            String new_ss = new_s.toString();
-            if(mp.containsKey(new_ss)){
-                if(mp.get(new_ss) != pattern.charAt(ptr1))
-                   return false;
-            }else{
-                mp.put(new_ss , pattern.charAt(ptr1));
-            }
-            if(map[pattern.charAt(ptr1) - 'a'] != null){
-                System.out.println(map[pattern.charAt(ptr1) - 'a'] + " " + new_ss);
-                if(map[pattern.charAt(ptr1) - 'a'].equals(new_ss) == false)
-                    return false;
-            }else{
-                map[pattern.charAt(ptr1) - 'a'] = new_ss;
-            }
-            
-            ++ptr1;
-        }
-        if(ptr1 < sz1 || ptr2 < sz2)
+        String[] words = s.split(" ");
+    if (words.length != pattern.length())
+        return false;
+    Map index = new HashMap();
+    for (Integer i=0; i<words.length; ++i)
+        if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
             return false;
-        
-        return true;
+    return true;
     }
 }
