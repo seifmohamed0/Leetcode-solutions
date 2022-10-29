bbc3c9353dd5ca042f58b3b5b66efd54983f73cd
seif mohamed <50503743+seifmohamed0@users.noreply.github.com>
2022-10-29 12:36:16 +0200

    Time: 1 ms (53.53%), Space: 42 MB (21.51%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.java
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.java
@@ -1,34 +1,24 @@
 class Solution {
     public int wiggleMaxLength(int[] nums) {
         int ret1 = 1 , ret2 = 1;
-        int prv = nums[0];
         boolean g = true;
         for(int i = 1 ; i < nums.length ; i++){
-            if(g == true && nums[i] > prv){
+            if(g == true && nums[i] > nums[i - 1]){
                 ++ret1;
                 g = false;
-                prv = nums[i];
-            }else if(g == false && nums[i] < prv){
+            }else if(g == false && nums[i] < nums[i - 1]){
                 ++ret1;
                 g = true;
-                prv = nums[i];
-            }else{
-                prv = nums[i];
             }
         }
-        prv = nums[0];
-         g = false;
+        g = false;
         for(int i = 1 ; i < nums.length ; i++){
-            if(g == true && nums[i] > prv){
+            if(g == true && nums[i] > nums[i - 1]){
                 ++ret2;
                 g = false;
-                prv = nums[i];
-            }else if(g == false && nums[i] < prv){
+            }else if(g == false && nums[i] < nums[i - 1]){
                 ++ret2;
                 g = true;
-                prv = nums[i];
-            }else{
-                prv = nums[i];
             }
         }
         return Math.max(ret1 , ret2);
